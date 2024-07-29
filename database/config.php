d4269227b2ae3c32d5e6d08e6f0881e30c89b5aa
Matheus Patricio da S e Silva <106037069+MatheusPatricioo@users.noreply.github.com>
2024-07-29 11:52:20 -0300

    update
    Upando novas atualizações na CRUD.

--- database/config.php
+++ database/config.php
@@ -1,8 +1,13 @@
 <?php
 
-$db_name = 'test';
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
+// Conexão com o banco de dados
+$db_name = 'test'; // Nome do banco de dados
+$db_host = 'localhost'; // Endereço do servidor do banco de dados
+$db_user = 'root'; // Usuário do banco de dados
+$db_pass = ''; // Senha do banco de dados
 
+
+ // Cria uma conexão PDO com o banco de dados MySQL
 $pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
+
+
